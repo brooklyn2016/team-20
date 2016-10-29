8f02b569cdf116c0ea30491d44bbd9b5f11436e0
Jacob Helfgot <jhelfgot1@gmail.com>
2016-10-29 01:23:39 -0400

    UI code
    This commit is the files for the ui with bootstrap

---
+++ mainJS.js
@@ -0,0 +1,27 @@
+console.log("script Loaded");
+
+var successfulUpload = false;
+document.getElementById("upload").addEventListener("click", function() {
+    var file = document.getElementById("uploadInput").value;
+    var eMail = document.getElementById("eMail").value;
+    console.log("Clicked");
+    console.log(file);
+    
+    
+    if (file == "" || eMail == "") {
+        console.log("File not uploaded");
+    }
+    else {
+        
+        console.log(file);
+        console.log(eMail);
+        localStorage.setItem("EMAIL", eMail);
+        successfulUpload = true;
+        
+    }
+    
+    if(successfulUpload) {
+        
+    }
+}, false);
+
