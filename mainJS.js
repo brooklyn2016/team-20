af78e342b679d729469db544a37d8d4b599d28ef
Jacob Helfgot <jhelfgot1@gmail.com>
2016-10-29 07:53:03 -0400

    Final Final Changes
    Final Changes For Real to the UI

--- mainJS.js
+++ mainJS.js
@@ -1,14 +1,12 @@
-console.log("script Loaded");
+
 
 var successfulUpload = false;
 document.getElementById("upload").addEventListener("click", function() {
     var file = document.getElementById("uploadInput").value;
-    console.log("Clicked");
-    console.log(file);
     
     
     if (file == "") {
-        console.log("File not uploaded");
+        successfulUpload = false;
     }
     else {
         
@@ -26,9 +24,7 @@ document.getElementById("upload").addEventListener("click", function() {
 document.getElementById("loginLI").addEventListener("click", function() {
     var unString = document.getElementById("loginUsername");
     var PWString = document.getElementById("loginPassword");
-    console.log("clicked");
     if (unString.value == "" || PWString.value == "") {
-        console.log("Here");
         document.getElementById("error").style.visibility = "visible";
     }
     else {
--- otherJS.js
+++ otherJS.js
@@ -4,8 +4,6 @@ document.getElementById("createAccount").addEventListener("click", function() {
     var unString = document.getElementById("userName").value;
     var pwString = document.getElementById("password").value;
     var checkBox = document.getElementById("terms")
-    console.log(fnString + lnString + unString+pwString);
-    console.log(checkBox.checked);
     if (!fnString || !lnString || !unString || !pwString || !checkBox.checked) {
         if (!checkBox.checked) {
             alert("Must accept terms of service!");
@@ -13,5 +11,8 @@ document.getElementById("createAccount").addEventListener("click", function() {
         else {
             alert("Must fill in all fields!");
         }
+    }
+    else {
+        window.location = "index.html";
     }
 }, false);
